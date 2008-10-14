d3291af2b0061ca0c3e8fcd1ea23b4372a9f76f3
ff11rcm <ff11rcm@20af9388-99b9-11dd-be93-c58765760c5f>
2008-10-14 06:34:09 +0000

    * 1st commit

---
+++ trunk/RCM/RCM/RCM.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using System.Threading;
+using FFXI;
+
+namespace RCM
+{
+    public partial class RCM : Form
+    {
+        public SortedList WindowerPids = new SortedList();
+        public Thread thread1;
+        public Thread thread2;
+        public Windower.MainFunctions w1;
+        public Windower.MainFunctions w2;
+
+        private RCMNetServer serv = null;
+        private RCMNetClient client = null;
+        
+        public RCM()
+        {
+            InitializeComponent();
+            ListWindowerProcess();
+            w1 = new Windower.MainFunctions();
+            w2 = new Windower.MainFunctions();
+        }
+
+        public void ListWindowerProcess()
+        {
+            WindowerPids.Clear();
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+            comboBox3.Items.Clear();
+            foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcessesByName("pol"))
+            {
+                WindowerPids.Add(p.MainWindowTitle, p);
+                comboBox1.Items.Add(p.MainWindowTitle);
+                comboBox2.Items.Add(p.MainWindowTitle);
+                comboBox3.Items.Add(p.MainWindowTitle);
+            }
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = comboBox2.Items.Count > 1 ? 1 : 0;
+            comboBox3.SelectedIndex = 0;
+        }
+
+        private string comRead(Windower.MainFunctions w)
+        {
+            string text = "";
+            short count;
+            string ret = string.Empty;
+            count = w.ConsoleGetArgCount();
+            text = w.ConsoleGetArg(0);
+            if (text == "rcm")
+            {
+                string[] args = new string[count - 1];
+                for (short i = 1; i < count; i++)
+                {
+                    args[i - 1] = w.ConsoleGetArg(i);
+                }
+                w.SendText("//rcmnop"); // send a dummy command (Required!!) // IsNewCommand がバグってるので
+                ret = String.Join(" ", args);
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// w1 から読み取ってw2に送る
+        /// ださいのでクラスつくったほうがいい
+        /// </summary>
+        public void comThread1()
+        {
+            string command;
+            while (true)
+            {
+                command = comRead(w1);
+                if (!String.IsNullOrEmpty(command))
+                {
+                    w2.SendText(command); // send command to w2
+                }
+                Thread.Sleep(50);
+            }
+        }
+
+        /// <summary>
+        /// w2 から読み取ってw1 に送る
+        /// </summary>
+        public void comThread2()
+        {
+            string command;
+            while (true)
+            {
+                command = comRead(w2);
+                if (!String.IsNullOrEmpty(command))
+                {
+                    w1.SendText(command); // send command to w2
+                }
+                Thread.Sleep(50);
+            }
+        }
+
+        public void RunP2()
+        {
+            System.Diagnostics.Process p;
+
+            p = (System.Diagnostics.Process)WindowerPids[comboBox1.SelectedItem];
+            w1.SetPID((uint)p.Id);
+            p = (System.Diagnostics.Process)WindowerPids[comboBox2.SelectedItem];
+            w2.SetPID((uint)p.Id);
+
+            ThreadStart t1 = new ThreadStart(comThread1);
+            thread1 = new Thread(t1);
+            thread1.IsBackground = true;
+            thread1.Start();
+            ThreadStart t2 = new ThreadStart(comThread2);
+            thread2 = new Thread(t2);
+            thread2.IsBackground = true;
+            thread2.Start();
+        }
+
+        public void StopP2()
+        {
+            thread1.Abort();
+            thread2.Abort();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "スタート")
+            {
+                button1.Text = "停止";
+                comboBox1.Enabled = false;
+                comboBox2.Enabled = false;                
+                groupBox2.Enabled = false;
+                RunP2();
+            }
+            else
+            {
+                StopP2();
+                button1.Text = "スタート";
+                comboBox1.Enabled = true;
+                comboBox2.Enabled = true;
+                groupBox2.Enabled = true;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "停止")
+                StopP2();
+            this.Close();
+        }
+
+        // Network Issues
+        // public functions.
+        public string getPass()
+        {
+            return textBoxPassword.Text;
+        }
+
+        public int getPort()
+        {
+            return (int)numericUpDown1.Value;
+        }
+
+        public string getAddress()
+        {
+            return textBoxAddress.Text;
+        }
+
+        public void WindowerSendText(string text)
+        {
+            // w1 をつかおう ownWindower とかにかえたほうがよさげ
+            w1.SendText(text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "スタート")
+            {
+                System.Diagnostics.Process p;
+                p = (System.Diagnostics.Process)WindowerPids[comboBox3.SelectedItem];
+                w1.SetPID((uint)p.Id);
+                button2.Text = "停止";
+                comboBox3.Enabled = false;
+                groupBox1.Enabled = false;
+                radioButton1.Enabled = false;
+                radioButton2.Enabled = false;
+                textBoxAddress.Enabled = false;
+                textBoxPassword.Enabled = false;
+                numericUpDown1.Enabled = false;
+                if (radioButton1.Checked)
+                {
+                    serv = new RCMNetServer(this);
+                    serv.Start();
+                    thread1 = new Thread(new ThreadStart(comNetThread));
+                    thread1.IsBackground = true;
+                    thread1.Start();
+                }
+                else
+                {
+                    client = new RCMNetClient(this);
+                    if (!client.Start())
+                    {
+                        MessageBox.Show(this, "接続に失敗しました。");
+                        enableNetGui();
+                    }
+                    else
+                    {
+                        thread1 = new Thread(new ThreadStart(comNetThread));
+                        thread1.IsBackground = true;
+                        thread1.Start();
+                    }
+                }
+            }
+            else
+            {
+                if (serv != null)
+                {
+                    serv.Stop();
+                    serv = null;
+                }
+                else
+                {
+                    client.Stop();
+                    client = null;
+                }
+                if (thread1 != null && thread1.IsAlive)
+                    thread1.Abort();
+                thread1 = null;
+                button2.Text = "スタート";
+                radioButton1.Enabled = true;
+                radioButton2.Enabled = true;
+                textBoxAddress.Enabled = true;
+                textBoxPassword.Enabled = true;
+                numericUpDown1.Enabled = true;
+                groupBox1.Enabled = true;
+            }
+        }
+
+        delegate void ArglessDelegateFunc();
+        private void enableNetGui()
+        {
+            if (InvokeRequired) {
+                Invoke(new ArglessDelegateFunc(enableNetGui));
+                return;
+            }
+            button2.Text = "スタート";
+            comboBox3.Enabled = true;
+            radioButton1.Enabled = true;
+            radioButton2.Enabled = true;
+            textBoxAddress.Enabled = true;
+            textBoxPassword.Enabled = true;
+            numericUpDown1.Enabled = true;
+            groupBox1.Enabled = true;     
+        }
+
+        public void clientDead()
+        {
+            if (thread1 != null && thread1.IsAlive)
+                thread1.Abort();
+            enableNetGui();
+        }
+
+        private void comNetThread()
+        {
+            string command;
+            while (true)
+            {
+                command = comRead(w1);
+                if (!string.IsNullOrEmpty(command))
+                {
+                    if (serv != null)
+                        serv.Enqueue(command); // <-- ?  SendText でいい気がする
+                    else if (client != null)
+                        client.SendText(command);
+                    w1.SendText("//rcmnop"); // IsNewCommand がバグってるので
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void updateListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ListWindowerProcess();
+        }
+    }
+}
---
+++ trunk/RCM/RCM/TeknicalzWindowerHelper.cs
@@ -0,0 +1,324 @@
+﻿// Windower Helper Class By: T3knicalD3ath
+// Version 1 Beta
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace FFXI
+{
+
+    public class Windower
+    {
+        #region "MainFunctions"
+
+        public class MainFunctions
+        {
+            //Keyboard Helper: (MMF Name = "WindowerMMFKeyboardHandler")
+            private  int mKeyboardHelper;
+            //Text Helper: (MMF Name = "WindowerMMFTextHandler")
+            private  int mTextHelper;
+            //Console Helper:
+            private  int mConsoleHandler;
+            //This Here Is Just A Simple Delay To Know How Long To Hold The Key Down For
+            private static int KeyDownDelay = 100;
+
+            public void SetPID(uint PID)
+            {
+                if (mTextHelper != 0)
+                DeleteTextHelper(mTextHelper);
+                if(mKeyboardHelper != 0)
+                DeleteKeyboardHelper(mKeyboardHelper);
+                if(mConsoleHandler != 0)
+                DeleteConsoleHelper(mConsoleHandler);
+                mTextHelper = CreateTextHelper("WindowerMMFTextHandler_" + PID.ToString());
+                mKeyboardHelper = CreateKeyboardHelper("WindowerMMFKeyboardHandler_" + PID.ToString());
+                mConsoleHandler = CreateConsoleHelper("WindowerMMFConsoleHandler_" + PID.ToString());
+            }
+
+            public void useLegacy()
+            {
+                DeleteTextHelper(mTextHelper);
+                DeleteKeyboardHelper(mKeyboardHelper);
+                DeleteConsoleHelper(mConsoleHandler);
+                mTextHelper = CreateTextHelper("WindowerMMFTextHandler");
+                mKeyboardHelper = CreateKeyboardHelper("WindowerMMFKeyboardHandler");
+                mConsoleHandler = CreateConsoleHelper("WindowerMMFConsoleHandler");
+            }
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern int CreateTextHelper(string name);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void DeleteTextHelper(int helper);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CTHCreateTextObject(int helper, string name);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CTHDeleteTextObject(int helper, string name);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CTHSetText(int helper, string name, string text);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CTHSetVisibility(int helper, string name, bool visible);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CTHSetFont(int helper, string name, ref byte font, short height);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CTHSetColor(int helper, string name, byte a, byte r, byte g, byte b);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CTHSetLocation(int helper, string name, float x, float y);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CTHSetBold(int helper, string name, bool bold);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CTHSetItalic(int helper, string name, bool italic);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CTHSetBGColor(int helper, string name, byte a, byte r, byte g, byte b);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CTHSetBGBorderSize(int helper, string name, float pixels);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CTHSetBGVisibility(int helper, string name, bool visible);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CTHSetRightJustified(int helper, string name, bool justified);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CTHFlushCommands(int helper);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern int CreateKeyboardHelper(string name);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void DeleteKeyboardHelper(int helper);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CKHSetKey(int helper, byte key, bool down);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CKHBlockInput(int helper, bool block);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CKHSendString(int helper, string data);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern int CreateConsoleHelper(string name);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void DeleteConsoleHelper(int helper);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern bool CCHIsNewCommand(int helper);
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern short CCHGetArgCount(int helper);
+            public  short ConsoleGetArgCount()
+            {
+                return CCHGetArgCount(mConsoleHandler);
+            }
+
+            [DllImport("WindowerHelper.dll")]
+            private static extern void CCHGetArg(int helper, short index, byte[] buffer);
+
+            public  string ConsoleGetArg(short index)
+            {
+                byte[] buffer = new byte[256];
+                // text = String.Format("{0:255}", " ");
+                CCHGetArg(mConsoleHandler, index, buffer);
+                var len = 0;
+                for (var i = 0; i < buffer.Length; i++)
+                {
+                    if (buffer[i] == 0)
+                    {
+                        len = i;
+                        break;
+                    }
+                }
+                return Encoding.Default.GetString(buffer, 0, len);
+            }
+
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //~~The Main Functions That Make Life Easy Start Here~~
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+            public  void SendText(string text)
+            {
+                CKHSendString(mKeyboardHelper, text);
+            }
+
+            public  void KeyPress(KeyCode KeyCode)
+            {
+                CKHSetKey(mKeyboardHelper, Convert.ToByte(KeyCode), true);
+                System.Threading.Thread.Sleep(KeyDownDelay);
+                CKHSetKey(mKeyboardHelper, Convert.ToByte(KeyCode), false);
+            }
+
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //~~~~~~~~~~~~~~~Works But I Don't Use It~~~~~~~~~~~~~~~
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            public  void KeyDown(KeyCode KeyCode, bool down) //to use set private to public
+            {
+                CKHSetKey(mKeyboardHelper, Convert.ToByte(KeyCode), down);
+            }
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        }
+
+        #endregion
+
+        #region "KeyCodes"
+
+        public enum KeyCode
+        {
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //~~~~~~~~~These Here Are The Most Important FFXI Keys~~~~~~~~~
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            EscapeKey = 1,
+
+            EnterKey = 28,
+            TabKey = 15,
+
+            UpArrow = 200,
+            LeftArrow = 203,
+            RightArrow = 205,
+            DownArrow = 208,
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //~~~~~~~These Here Are The NumPad Keys On Your Keyboard~~~~~~~
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            MainNumlockKey = 69,
+
+            NP_Number0 = 82,
+            NP_Number1 = 79,
+            NP_Number2 = 80,
+            NP_Number3 = 81,
+            NP_Number4 = 75,
+            NP_Number5 = 76,
+            NP_Number6 = 77,
+            NP_Number7 = 71,
+            NP_Number8 = 72,
+            NP_Number9 = 73,
+
+            NP_Forwardslash = 181,
+            NP_MultiplyKey = 55,
+            NP_MinusKey = 74,
+            NP_AdditionKey = 78,
+            NP_EnterKey = 156,
+            NP_PeriodKey = 83,
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //~~~These Here Are The Letters From A to Z On Your Keyboard~~~
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            LetterA = 30,
+            LetterB = 48,
+            LetterC = 46,
+            LetterD = 32,
+            LetterE = 18,
+            LetterF = 33,
+            LetterG = 34,
+            LetterH = 35,
+            LetterI = 23,
+            LetterJ = 36,
+            LetterK = 37,
+            LetterL = 38,
+            LetterM = 50,
+            LetterN = 49,
+            LetterO = 24,
+            LetterP = 25,
+            LetterQ = 16,
+            LetterR = 19,
+            LetterS = 31,
+            LetterT = 20,
+            LetterU = 22,
+            LetterV = 47,
+            LetterW = 17,
+            LetterX = 45,
+            LetterY = 21,
+            LetterZ = 44,
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //~~~These Here Are The Numbers From 0 to 9 On Your Keyboard~~~
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            Number1 = 2,
+            Number2 = 3,
+            Number3 = 4,
+            Number4 = 5,
+            Number5 = 6,
+            Number6 = 7,
+            Number7 = 8,
+            Number8 = 9,
+            Number9 = 10,
+            Number0 = 11,
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //~~These Here Are The F Keys From F1 to F12 On Your Keyboard~~
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            F1Key = 59,
+            F2Key = 60,
+            F3Key = 61,
+            F4Key = 62,
+            F5Key = 63,
+            F6Key = 64,
+            F7Key = 65,
+            F8Key = 66,
+            F9Key = 67,
+            F10Key = 68,
+            F11Key = 87,
+            F12Key = 88,
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //~~~~These Here Are Ones That You Should Not Need But Here~~~~
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //MinusKey = 12,
+            //EqualsKey = 13,
+            //BackspaceKey = 14,
+            //LeftBracket = 26,
+            //RightBracket = 27,
+            //LeftCtrlKey = 29,
+            //Semicolon = 39,
+            //Apostrophe = 40,
+            //Accentgrave = 41,
+            //LeftShift = 42,
+            //Backslash = 43,
+            //CommaKey = 51,
+            //PeriodKey = 52,
+            //ForwardslashKEy = 53,
+            //RightShift = 54,
+            //ScrollLock = 70,
+            //LeftAltKey = 56,
+            //Spacebar = 57,
+            //CapsLock = 58,
+            //RightControlKey = 157,
+            //RightAltKey = 184,
+            //InsertKey = 210,
+            //DeleteKey = 211,
+            //LeftWindowKey = 219,
+            //RightWindowKey = 220
+
+            //Calculator = &HA1
+
+            //MuteKey = &HA0
+            //PlayNPauseKey = &HA2
+            //StopMedia = &HA
+            //VolumeDown = &HAE
+            //VolumeUp = &HB0
+            //NextMediaTrack = &HED
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        }
+
+        #endregion
+    }
+}
