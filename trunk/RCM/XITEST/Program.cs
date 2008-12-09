7f8e2a7b87befa2a945288917774d24b94847385
ff11rcm <ff11rcm@20af9388-99b9-11dd-be93-c58765760c5f>
2008-12-09 13:20:41 +0000

    * added more test.

--- trunk/RCM/XITEST/Program.cs
+++ trunk/RCM/XITEST/Program.cs
@@ -35,8 +35,12 @@ namespace XITEST
 
         static void Pause()
         {
-            Console.Write("何かキーを押すと続けます...");
-            Console.ReadKey();
+            ConsoleKeyInfo cki;
+            Console.Write("何かキーを押すと続けます... (ESCで終了)");
+            cki = Console.ReadKey();
+            if (cki.Key == ConsoleKey.Escape)
+                Environment.Exit(0);
+            Console.WriteLine();
         }
 
         static void CheckPlayer(FFXI.XIACE.Player player)
@@ -50,6 +54,28 @@ namespace XITEST
             Console.WriteLine("Activity: {0} (ID: 0x{1:X})", player.Activity, (int)player.Activity);
             Console.WriteLine();
 ;
+        }
+
+        static void CheckEquipment(FFXI.XIACE.Inventory inventory)
+        {
+            Console.WriteLine("== Checking Inventory Class : Equipment ==");
+            Console.WriteLine("Main: {0}", inventory.GetEquippedItemName(eEquipSlot.Main));
+            Console.WriteLine("Sub: {0}", inventory.GetEquippedItemName(eEquipSlot.Sub));
+            Console.WriteLine("Range: {0}", inventory.GetEquippedItemName(eEquipSlot.Range));
+            Console.WriteLine("Ammo: {0}", inventory.GetEquippedItemName(eEquipSlot.Ammo));
+            Console.WriteLine("Head: {0}", inventory.GetEquippedItemName(eEquipSlot.Head));
+            Console.WriteLine("Body: {0}", inventory.GetEquippedItemName(eEquipSlot.Body));
+            Console.WriteLine("Hands: {0}", inventory.GetEquippedItemName(eEquipSlot.Hands));
+            Console.WriteLine("Legs: {0}", inventory.GetEquippedItemName(eEquipSlot.Legs));
+            Console.WriteLine("Feet: {0}", inventory.GetEquippedItemName(eEquipSlot.Feet));
+            Console.WriteLine("Waist: {0}", inventory.GetEquippedItemName(eEquipSlot.Waist));
+            Console.WriteLine("Neck: {0}", inventory.GetEquippedItemName(eEquipSlot.Neck));
+            Console.WriteLine("Ear Left: {0}", inventory.GetEquippedItemName(eEquipSlot.EarLeft));
+            Console.WriteLine("Ear Right: {0}", inventory.GetEquippedItemName(eEquipSlot.EarRight));
+            Console.WriteLine("Ring Left: {0}", inventory.GetEquippedItemName(eEquipSlot.RingLeft));
+            Console.WriteLine("Ring Right: {0}", inventory.GetEquippedItemName(eEquipSlot.RingRight));
+            Console.WriteLine("Back: {0}", inventory.GetEquippedItemName(eEquipSlot.Back));
+            Console.WriteLine();
         }
 
         static void CheckInventory(FFXI.XIACE.Inventory inventory)
@@ -72,21 +98,47 @@ namespace XITEST
                 name = inventory.GetItemNameById(item.id);
                 Console.WriteLine(" {0,2:D2}: {1:X4} {2} x{3}", i, (int)item.id, String.IsNullOrEmpty(name) ? "EMPTY" : name, item.count);
             }
+            Pause();
+            Console.WriteLine("Storage: {0}", inventory.GetStorageMax());
+            for (short i = 0; i < inventory.GetStorageMax(); i++)
+            {
+                string name;
+                Inventory.InventoryItem item = inventory.GetStorageItem(i);
+                name = inventory.GetItemNameById(item.id);
+                Console.WriteLine(" {0,2:D2}: {1:X4} {2} x{3}", i, (int)item.id, String.IsNullOrEmpty(name) ? "EMPTY" : name, item.count);
+            }
+            Pause();
+            Console.WriteLine("Locker: {0}", inventory.GetLockerMax());
+            for (short i = 0; i < inventory.GetLockerMax(); i++)
+            {
+                string name;
+                Inventory.InventoryItem item = inventory.GetLockerItem(i);
+                name = inventory.GetItemNameById(item.id);
+                Console.WriteLine(" {0,2:D2}: {1:X4} {2} x{3}", i, (int)item.id, String.IsNullOrEmpty(name) ? "EMPTY" : name, item.count);
+            }
             Console.WriteLine();
         }
 
         static void Main(string[] args)
         {
             Process p = CheckFFXIRunning();
-            XIWindower xiw;
+            XIWindower xiw = null;
 
             if (p == null)
             {
                 Finish("FFXi is not running.");
             }
-            
-            xiw = new XIWindower(p.Id);
+            try
+            {
+                xiw = new XIWindower(p.Id);
+            }
+            catch
+            {
+                Finish("WindowerHelper.dll がないかも");
+            }
             CheckPlayer(xiw.Player);
+            Pause();
+            CheckEquipment(xiw.Inventory);
             Pause();
             CheckInventory(xiw.Inventory);
 
